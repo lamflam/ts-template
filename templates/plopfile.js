0e241d60e000ceeca9ac847ba91822d71288d72b
Kevin LaFlamme <kevinl@hey.com>
2022-01-31 10:25:41 -0500

    Add generators for hooks and components

--- templates/plopfile.js
+++ templates/plopfile.js
@@ -16,4 +16,40 @@ module.exports = (plop) => {
             },
         ],
     });
+
+    plop.setGenerator('component', {
+        description: 'Create a new component or module',
+        prompts: [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Enter component name: ',
+            },
+        ],
+        actions: [
+            {
+                type: 'addMany',
+                destination: '../src/{{name}}',
+                templateFiles: 'component/*.hbs',
+            },
+        ],
+    });
+
+    plop.setGenerator('hook', {
+        description: 'Create a new hook',
+        prompts: [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Enter hook name: ',
+            },
+        ],
+        actions: [
+            {
+                type: 'addMany',
+                destination: '../src/{{name}}',
+                templateFiles: 'hook/*.hbs',
+            },
+        ],
+    });
 };
